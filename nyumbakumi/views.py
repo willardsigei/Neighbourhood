95d2e990a0b65c302150c419866e455d001586e2
willardsigei <kipngeno.sigei@student.moringaschool.com>
2022-01-06 23:45:28 +0300

    views > registration func

--- nyumbakumi/views.py
+++ nyumbakumi/views.py
@@ -8,3 +8,29 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request,'index.html')
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
+
+
