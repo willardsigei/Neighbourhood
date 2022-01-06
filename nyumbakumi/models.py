a66b79560fe6791ab3fdf59262f74cb8ccc0a7d9
willardsigei <kipngeno.sigei@student.moringaschool.com>
2022-01-06 23:08:28 +0300

    created class model neighbourhood

--- nyumbakumi/models.py
+++ nyumbakumi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+import datetime as dt
+from django.db.models import Q
 
 # Create your models here.
+
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
+
