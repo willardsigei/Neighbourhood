47f9a45b21f7d3ec7bcfc0413d9017d6ff1027e4
willardsigei <kipngeno.sigei@student.moringaschool.com>
2022-01-06 23:43:22 +0300

    views > imports

--- nyumbakumi/views.py
+++ nyumbakumi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
