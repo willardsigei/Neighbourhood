8ff7a114ec182f441ecb6d476e9892a76409f84c
willardsigei <kipngeno.sigei@student.moringaschool.com>
2022-01-06 23:13:53 +0300

    created profile model

--- nyumbakumi/models.py
+++ nyumbakumi/models.py
@@ -19,3 +19,14 @@ class neighbourhood(models.Model):
     def delete_neighbourhood(cls,neighbourhood):
         cls.objects.filter(neighbourhood=neighbourhood).delete()
 
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
