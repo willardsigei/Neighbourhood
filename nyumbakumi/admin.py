d17a18ff9e4945f810f213cf5ff540655b345e0b
willardsigei <kipngeno.sigei@student.moringaschool.com>
2022-01-06 22:57:40 +0300

    site admin

--- nyumbakumi/admin.py
+++ nyumbakumi/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import neighbourhood, Profile, Authorities, Health, Post
+
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Authorities)
+admin.site.register(Health)
+admin.site.register(Post)
